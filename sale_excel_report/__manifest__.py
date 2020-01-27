a5b5ad37e6b3c2d6565626c70390ef80367e5a85
Murugan Chinnasamy <murugan@think42labs.com>
2020-01-27 10:30:24 +0530

    refs # new module

---
+++ sale_excel_report/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Quotation / Sale Excel Report',
+    'version': '1.0',
+    'summary': 'Quotation / Sale Excel Report',
+    'description': """
+Quotation / Sale Excel Report
+==========================
+    """,
+    'author': "Murugan",
+    'website': "http://www.cmstech.in",
+    'category': 'Users',
+    'license': "OPL-1",
+    'complexity': 'easy',
+    'depends': ['base','sale'],
+    'data': [
+            'views/excel_view.xml',
+            ],
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'application': True
+}
