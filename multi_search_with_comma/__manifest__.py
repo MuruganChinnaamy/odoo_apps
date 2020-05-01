561d79c9f88069e832b5314ba98b5c9043d49307
Murugan Chinnasamy <murugan@think42labs.com>
2020-05-01 10:16:53 +0530

    refs # new module for multi search with comma

---
+++ multi_search_with_comma/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Multi Search with comma separated',
+    'version': '11.0',
+    'category' : 'General',
+     'summary' : """ Records bulk search on any page/model.
+    """,
+    'description': """
+                Search for records in bulks on any page/model easily.
+    """,
+    'author' : 'Murugan Chinnasamy',
+    'website': 'www.cmstech.com',
+    'depends': ['base'],
+    'init_xml': [],
+    'data': [
+             ], 
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
